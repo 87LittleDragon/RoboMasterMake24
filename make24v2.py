ce457dcade3f451afb520fd8fe5904e4f86ae1b6
Gavin YIP <gavinyip@Gavin-YIP.local>
2024-06-21 17:46:11 +0800

    fixed bug, now funtion make24 perimiter are passes by list

--- make24v2.py
+++ make24v2.py
@@ -1,42 +1,42 @@
-def make24(a,b,c,d,sign):
-    numbers = [a,b,c,d]
-    if sign == "+":
+def make24(numberList):
+    numbers = numberList[:4]
+    if numberList[4] == 40:
         for i in range(2,-1,-1):
+            if i == 0:
+                i = ""
+                toShoot = []
+            elif i in numbers:
+                toShoot = [i]
+                numbers[numbers.index(i)] = None
+            else:
+                continue
             for ii in range(9, -1, -1):
-                if i == 0:
-                    i = ""
-                    toShoot = []
-                elif i in numbers:
-                    toShoot = [numbers.index(i) + 1]
-                    numbers[numbers.index(i)] = None
-                else:
-                    continue
                 if not ii in numbers:
                     continue
                 temp = str(i) + str(ii)
                 remain = 24 - int(temp)
                 if remain < 0:
                     continue
-                toShoot.append(numbers.index(ii) + 1)
+                toShoot.append(ii)
                 numbers[numbers.index(ii)] = None
                 if remain in numbers:
-                    toShoot.append(0)
-                    toShoot.append(numbers.index(remain) + 1)
+                    toShoot.append(numberList[4])
+                    toShoot.append(remain)
                     return toShoot
                 for iii in range(remain, -1, -1):
                     if iii not in numbers:
                         continue
-                    toShoot.append(0)
-                    toShoot.append(numbers.index(iii) + 1)
+                    toShoot.append(numberList[4])
+                    toShoot.append(iii)
                     numbers[numbers.index(iii)] = None
                     if remain - iii in numbers:
-                        toShoot.append(0)
-                        toShoot.append(numbers.index(remain - iii) + 1)
+                        toShoot.append(numberList[4])
+                        toShoot.append(remain - iii)
                         return toShoot
-                
-                numbers = [a,b,c,d]
+                toShoot.pop(-1)
+                numbers = numberList[:4]
 
     
     
 
-print(make24(3,9,7,8,"-"))
+print(make24([1,2,3,4,40]))
